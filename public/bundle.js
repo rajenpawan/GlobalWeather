82839c313930bdddb2e79ea0ecf30e589d12fd28
Rajendra <rajen.pawan@gmail.com>
2017-05-26 22:25:17 +0530

    Added copy

--- public/bundle.js
+++ public/bundle.js
@@ -25591,9 +25591,18 @@
 
 	var Examples = function Examples(props) {
 	  return React.createElement(
-	    'h3',
+	    'div',
 	    null,
-	    'Examples Component!'
+	    React.createElement(
+	      'h3',
+	      null,
+	      'Examples Component!'
+	    ),
+	    React.createElement(
+	      'p',
+	      null,
+	      'Welcome to Examples page!'
+	    )
 	  );
 	};
 
