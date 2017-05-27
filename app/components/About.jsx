c36341b7136e55c04a004d88f33a9b8058cf93f9
Rajendra <rajen.pawan@gmail.com>
2017-05-27 22:07:05 +0530

    Update about Page copy

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 var About = (props) =>{
   return(
-    <h3>Welcome to the About page</h3>
+    <div>
+<h3>About</h3>
+<p>Welcome to the About Page</p>
+    </div>
   )
 };
 // var About = React.createClass({
