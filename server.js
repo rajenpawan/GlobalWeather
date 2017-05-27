e5bc0e894bfca6335c7f4b33d75f1deec09bfe7b
Rajendra <rajen.pawan@gmail.com>
2017-05-27 12:58:46 +0530

    prep repo for Heroku

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var pawan = express();
+const PORT = process.env.PORT || 3000;
+
+pawan.use(function (req, res, next){
+if(req.headers['x-forwarded-proto'] === 'http'){
+  next();
+  else{
+    res.redirect('http://' + req.hostname + req.url);
+  }
+}
+});
+
 pawan.use(express.static('public'));
-pawan.listen(3000, function(){
-  console.log('Express Server is Up on Port 3000');
+pawan.listen(PORT, function(){
+  console.log('Express Server is Up on Port ' + PORT);
 });
