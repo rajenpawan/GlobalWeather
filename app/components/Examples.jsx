3ff3ebba456b878b4b3bac1bf193d1dc01d7f88f
Rajendra <rajen.pawan@gmail.com>
2017-05-28 17:09:31 +0530

    Changes in Examples & main

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,21 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return(
     <div>
-<h3>Examples</h3>
-<p>Welcome to Examples page!</p>
-    </div>
+<h1 className="text-center">Examples</h1>
+<p>Here are a few example Locations to Try out!</p>
+<ol>
+<li>
+<Link to='/?location=Bangalore'>Bangalore, Sudhamanagar</Link>
+</li>
+<li>
+  <Link to='/?location=Rio'>Rio, Brazil</Link>
+</li>
+</ol>
+
+</div>
   )
 };
 
